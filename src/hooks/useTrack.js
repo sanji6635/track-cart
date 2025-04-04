6b6ade6d57423b0f8d4c8d48343f100c9cbb945c
sanji <sanji@gmail.com>
2025-04-04 04:28:34 +0000

    changed the backend url

--- src/hooks/useTrack.js
+++ src/hooks/useTrack.js
@@ -1,13 +1,17 @@
 const useTrack = () => {
   try {
     const track = async (trackingNumber) => {
-      const responce = await fetch("http://localhost:5000/api/track", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ trackingNumber: trackingNumber }),
-      });
+      const responce = await fetch(
+        "https://track-cart-backend.onrender.com/api/track",
+        {
+          //it was previously "http://localhost:5000/api/track" now; changed due to render
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ trackingNumber: trackingNumber }),
+        }
+      );
       if (responce.error) {
         throw new Error(responce.error);
       }
